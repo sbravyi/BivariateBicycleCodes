e36382079a5ba5b56bebf4b664e580df24aa69a9
alexandru paler <alexandrupaler@gmail.com>
2024-06-26 15:25:16 +0300

    formatting

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ python3.10 -m venv venvs
 source venv/bin/activate
 
 pip install cython==0.29.37
-pip install wheels
+pip install wheel
 pip install numpy==1.22.0
 pip install scipy==1.8.0
 pip install git+https://github.com/quantumgizmos/ldpc.git --no-build-isolation
